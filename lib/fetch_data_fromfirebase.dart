18e3ef31d8db04c1a877e60e6dcac1401cd45b49
alyamnor <noralya@graduate.utm.my>
2025-04-22 02:49:45 +0800

    Curved Bottom Navigation Bar

--- lib/fetch_data_fromfirebase.dart
+++ lib/fetch_data_fromfirebase.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+
 
 class BookGridByCategory extends StatefulWidget {
   const BookGridByCategory({super.key});
@@ -36,7 +38,19 @@ class _BookGridByCategoryState extends State<BookGridByCategory> {
         ),
         centerTitle: true,
       ),
-      body: SafeArea(
+        bottomNavigationBar: CurvedNavigationBar(
+    backgroundColor: Colors.transparent,
+    color: const Color(0xFFE5D3B3),
+    items: <Widget>[
+      Icon(Icons.home_filled, size: 30, color: Color(0xFF987554),),
+      Icon(Icons.add_a_photo_sharp, size: 30, color: Color(0xFF987554)),
+      Icon(Icons.person_2, size: 30, color: Color(0xFF987554)),
+    ],
+    onTap: (index) {
+      //Handle button tap
+    },
+  ),
+  body: SafeArea(
         child: Column(
           children: [
             // Search Bar
